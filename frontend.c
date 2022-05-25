2488d7102e0a386aa053b69805ed14714d48c59a
Steven Wong <syw14@sfu.ca>
2022-05-24 19:12:22 -0700

    create snake ncurses window

--- frontend.c
+++ frontend.c
@@ -6,35 +6,33 @@
 // will include all the drawing of the snake game onto terminal using the ncurses library 
 // ncurses documentation ----->>>>> https://jbwyatt.com/ncurses.html#using
 
+// To Do
+// Print menu with the following options: Start game, how to play, high scores 
+// Print menu of how to play: Move snake with the arrow keys, Eat the food to grow, Eat the apple to double speed temporarily for 30s and grow events are doubled 
+// Print high scores: Retro style just like in the old arcades 
+// Draw fruit items randomly within the screen 
+// Draw Bonus Item Sportically within the screen 
+
+
 #include <stdio.h> 
 #include <ncurses.h>
 
-// Print menu with the following options: Start game, how to play, high scores 
-
-// Print menu of how to play: Move snake with the arrow keys, Eat the food to grow, Eat the apple to double speed temporarily for 30s and grow events are doubled 
-
-
-// Print high scores: Retro style just like in the old arcades 
-
-
-
-
-// WINDOW * win = newwin(25, 25, 0, 0)  // nlines, ncols, y0, x0
-
-
 // Test creating a window in ncurses
-int main(void) { 
-   initscr();
-   addstr("hah \n ahah\n");
-   refresh(); 
-   getch();
+int main(int argc, char **argv) { 
+   initscr();  // initialize ncurses
+   refresh();  // print onto screen
+
+   // get dimensions of window
+   int screenX, screenY;  
+   getmaxyx(stdscr, screenY, screenX);
+    
+   // initialize board in centre of screen
+   WINDOW *board_win = newwin(20, 20, (screenY/2)-20/2, (screenX/2)-20/2);  // height, width, row, column to centre window
+   box(board_win, '|', '_');  // board, verticalChar, horizontalChar
+   wrefresh(board_win);
+
+   getch();  // test only
    endwin(); 
 
    return 0;
 }
-
-// Draw fruit items randomly within the screen 
-
-
-
-// Draw Bonus Item Sportically within the screen 
