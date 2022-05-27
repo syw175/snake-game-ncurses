520edf881015c1b085055fe46084a527b0fbbf75
Steven Wong <syw14@sfu.ca>
2022-05-27 15:42:52 -0700

    Printing of menu and game instructions

--- frontend.c
+++ frontend.c
@@ -16,6 +16,8 @@
 
 #include <stdio.h> 
 #include <ncurses.h>
+#include <string.h>
+
 
    // move the cursor to the specified location, taking 2 parameters y, then x 
    // move(20, 20);  // 
@@ -35,57 +37,8 @@
    // print text in the window at the current position
    // wprintw(menuWin, "This is a window");
 
-
-int main(int argc, char **argv) { 
-
-   // NCURSES START 
-   initscr();  // initialize ncurses, sets up memory, and clears the screen
-   noecho();   // character from input will not show up 
-   cbreak();  
-   refresh();  // refresh screen to match what is in memory
-
-   // clears the screen
-   clear();
-
-   // get dimensions of window
-   int screenX, screenY;  
-   getmaxyx(stdscr, screenY, screenX);
-    
-
-
-   // initialize board in centre of screen
-   // new pointer to new window named menuWin
-   WINDOW *menuWin = newwin(screenY, screenX, 0, 0);  // height, width, row location, column location to centre window
-   refresh();
-
-
-   // board, verticalChar, horizontalChar... also takes the int representation of the key. I.e. 103 = g char
-   box(menuWin, '|', '_');  
-
-   mvprintw(screenY/3, screenX/3, "Welcome to Snake Game!");
-
-
-   string menuOptions[3] = {
-      "START GAME"
-      "HOW TO PLAY"
-      "SCORE BOARD"
-   };
-
-   int choice;
-   int highlight = 0;
-
-   for (int i = 0; i < MENUOPTIONS; i++) { 
-      mvprintw(menuWin, posy+i+5, posx+i+5, menuOptions[i].c_str());
-      wrefresh(menuWin);
-   }
-
-
-   wrefresh(menuWin);  // update box to reflect what is in memory
-
-   // allow ncurses to take function keys and arrow keys
-   keypad(menuWin, true);
-
-
+   // // clears the screen
+   // clear();
 
    // int c = getch();  // get char... waits for user input, returns int value of that key...
    // printw("%d", c);
@@ -95,11 +48,105 @@ int main(int argc, char **argv) {
       // do something
    // }
 
+
+void mainMenu() { 
+
+   // Ncurses start 
+   clear();
+   initscr();  // initialize ncurses, sets up memory, and clears the screen
+   noecho();   // character from input will not show up 
+   cbreak();  
+
+   // Get dimensions of screen
+   int screenX, screenY;  
+   getmaxyx(stdscr, screenY, screenX);
+
+   // Initialize game board
+   mvprintw(screenY/3, screenX/3, "WELCOME TO SNAKE GAME!");
+   WINDOW *menuWin = newwin(screenY, screenX, 0, 0);  // height, width, row location, column location to centre window
+   box(menuWin, '|', '_');  
+   refresh();
+   getch();
+   wrefresh(menuWin);
    getch();
 
-   
+
+   keypad(menuWin, true);
+
+   char *menuOptions[] = {"START GAME", "HOW TO PLAY", "SCORE BOARD"};
+   int highlighted = 0;
+
+   while(true) {
+      for (int i = 0; i < 3; i++) { 
+         if(i == highlighted) { 
+            wattron(menuWin, A_REVERSE);
+         }
+
+         mvwprintw(menuWin, i+1, 1, menuOptions[i]);
+         wattroff(menuWin, A_REVERSE);
+      }
+      int userInput = wgetch(menuWin);  // wgetch(menuWin) also refreshes screen here
+
+      switch (userInput)
+      {
+         case KEY_UP:
+            if (highlighted > 0) { 
+               highlighted--;
+            }
+            break;
+         case KEY_DOWN:  
+            if (highlighted < 2) {
+               highlighted++;
+            } 
+            break;
+         default:
+            break;
+      }
+
+      if (userInput == 10) { 
+         break;
+      }
+   }
+
+   getch();
    // NCURSES END
    endwin();  //deallocates memory and ends ncurses...
+}
+
+void instructionsMenu(void) { 
+
+   initscr();
+   noecho();
+   cbreak();
+
+   // Initialize instructions screen
+   int yMax, xMax; 
+   getyx(stdscr, &yMax, &xMax);
+
+   // int height, int width, int starty, int startx
+   WINDOW *instructionsMenu = new(yMax, xMax, 0, 0);
+   refresh();
+   box(instructionsMenu, 0, 0);
+   wrefresh(instructionsMenu);
+
+
+   char *gameInstructions[] = {"HOW TO PLAY",
+                        "1. MOVE WITH THE ARROW KEYS", 
+                        "2. EAT THE FOOD (#) TO GROW",
+                        "3. BONUS ITEM DOUBLES FOOD AND SPEED FOR 60SEC",
+                        "PRESS ESC TO RETURN"};
+
+
+   int listSize = sizeof(gameInstructions) / gameInstructions[0];
+   for (int i = 0; i < gameInstructions)
+
+   
+   getch();
+}
+
+int main(int argc, char **argv) { 
+   mainMenu();   
+   instructionsMenu();
 
    return 0;
 }
