f3c5cef550e4ca2ba4464a6f0cb7e0d4faa08b21
Steven Wong <syw14@sfu.ca>
2022-08-30 15:18:38 -0700

    header file for frontend.c reorganized

--- src/frontend.h
+++ src/frontend.h
@@ -5,29 +5,47 @@
  *              of the snake game in terms of the MVC design pattern.
  * 
  * Author: Steven Wong
- * Last Modified: August 27, 2022
+ * Last Modified: August 30, 2022
  */
 
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <panel.h> 
+#include <menu.h>
 #include <stdio.h> 
 #include <ncurses.h>
 #include <string.h>
 
 
-// Add a char at a given x and y position
-void addElement(WINDOW *board, char element, int xPosition, int yPosition);
+// Initialize the game board onto the heap and return a pointer to it
+WINDOW* initializeWindow(int height, int width, int centreY, int centreX); 
 
-// Initialize the game board
-WINDOW* initializeWindow(); 
+// Draw the main menu information and loop until an option is chosen
+void drawMainMenu(WINDOW *board);
+
+// Draw the gameplay instructions onto the screen and return to menu upon pressing 1
+void drawInstructionsMenu(WINDOW *board);
+
+// Draw the historical record of highscores located in the current directory
+void drawHighScoresMenu(WINDOW *board);
 
 // Clear the screen and redraw the borders
 void clearBoard(WINDOW *board);
 
-// Draw the main menu information and loop until an option is chosen
-void drawMenu(WINDOW *board);
+// Exit the game 
+void exitGame(void);
 
-// Draw the gameplay instructions onto the screen and return to menu upon pressing 1
-void drawInstructions(WINDOW *board);
+// Start the main game loop
+void startGame(WINDOW *board);
+
+// Print a string in the middle of the window
+void printStringInMiddle(WINDOW *win, int centreY, int centreX, char* string);
+
+// Get the current Direction
+direction_t getDirection(direction_t currentDirection);
+
+// Refresh the board and display its contents
+void refreshBoard(board_t *board, WINDOW *win);
 
 // Get input from user and return a chtype
 chtype getInput(WINDOW *board);
